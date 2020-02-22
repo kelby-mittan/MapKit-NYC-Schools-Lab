96ad18ffe9eeb8162436995060a2c73c6030beee
Kelby Mittan <kelbymittan@pursuit.org>
2020-02-22 17:00:22 -0500

    Initial Commit

---
+++ MapKit-NYC-Schools-Lab/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKit-NYC-Schools-Lab
+//
+//  Created by Kelby Mittan on 2/22/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
